654bed84bc4b061d195382e8670c6f11807fb76b
xtijohn <ypoulopoulos@hotmail.com>
2019-11-14 15:23:18 +0200

    Deleted height

--- Student.cs
+++ Student.cs
@@ -7,6 +7,5 @@ namespace Demo_Website{
         public string FirstName { get; set;}
         public string LastName { get; set;}
         public DateTime BirthDate { get; set; }
-        public double Height {get;set;}
     }
 }
