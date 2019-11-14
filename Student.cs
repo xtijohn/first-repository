9bd8dec9af416eba4d21ba8dad1b25658d9f7a94
xtijohn <ypoulopoulos@hotmail.com>
2019-11-14 15:07:50 +0200

    Created Person class

---
+++ Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Demo_Website{
+    public class Student{
+
+        public int Id{ get; set;}
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+
+    }
+
+}
